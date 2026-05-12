b701247255cd705e0a44f0f704f28aeb8db5b7a5
small-python <ahmedyinusa2004@gmail.com>
2026-05-12 02:54:01 +0100

    initial project setup

---
+++ src/main/java/com/ahmed/bookmark/BookmarkApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkApiApplication.class, args);
+	}
+
+}
